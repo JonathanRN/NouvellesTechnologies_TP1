913f3af198771acf3092ea057a7cc81f7ea633ac
JonathanRN <minijona7@gmail.com>
2018-09-13 19:45:56 -0400

    mfc

--- Assets/Scripts/Builder/HunterBuilder.cs
+++ Assets/Scripts/Builder/HunterBuilder.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HunterBuilder : CharacterBuilder {
-
+public class HunterBuilder : CharacterBuilder 
+{
 	private Character character;
     
 	public HunterBuilder()
